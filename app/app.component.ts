a576dc775eb30887c02422216834928b4b549ce3
mmotel <mateuszmotel@gmail.com>
2015-12-21 21:16:46 +0100

    Part 4: Services
    * added loading animation (getHeroesSlowly).

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,31 @@
 import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroListComponent} from './hero-list.component';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: '../templates/app.component.html',
-  directives: [HeroDetailComponent, HeroListComponent]
+  directives: [HeroDetailComponent, HeroListComponent],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title :string = 'Tour of Heroes';
   public subtitle :string = 'The best Heroes in your city!';
-  public heroes :Hero[] = HEROES;
+  public heroes :Hero[];
   public selectedHero :Hero;
+  private _heroService :HeroService;
+
+  constructor (service :HeroService) {
+    this._heroService = service;
+  }
+
+  ngOnInit () {
+    this.getHeroes();
+  }
 
   onSelected (hero) {
     this.selectedHero = hero;
@@ -21,18 +34,8 @@ export class AppComponent {
   onUnselected () {
     this.selectedHero = null;
   }
+
+  getHeroes () {
+    this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
 }
-
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-]

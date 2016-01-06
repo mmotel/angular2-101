a694ff0082fef82d84503868f77aa174246e5b8f
mmotel <mateuszmotel@gmail.com>
2016-01-06 17:37:41 +0100

    adjusted hero-list to work with router

--- app/heroes/component/hero-list.component.ts
+++ app/heroes/component/hero-list.component.ts
@@ -1,14 +1,24 @@
-import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {Component, Input, Output, EventEmitter, OnInit} from 'angular2/core';
 import {Hero} from '../model/hero';
+import {HeroService} from '../service/hero.service';
 
 @Component({
   selector: 'my-hero-list',
-  templateUrl: './app/heroes/template/hero-list.component.html'
+  templateUrl: './app/heroes/template/hero-list.component.html',
+  providers: [HeroService]
 })
-export class HeroListComponent {
-  @Input() heroes :Hero[];
-  @Input() selectedHero :Hero;
-  @Output() selectedHeroChange :EventEmitter<Hero> = new EventEmitter();
+export class HeroListComponent implements OnInit {
+  private heroes :Hero[];
+  private selectedHero :Hero;
+  private _heroService :HeroService;
+
+  constructor (heroService :HeroService) {
+    this._heroService = heroService;
+  }
+
+  ngOnInit () {
+    this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
 
   onSelect (hero :Hero) {
     if (this.selectedHero === hero) {
@@ -16,6 +26,5 @@ export class HeroListComponent {
     } else {
       this.selectedHero = hero;
     }
-    this.selectedHeroChange.emit(this.selectedHero);
   }
 }
